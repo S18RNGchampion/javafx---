817e840b5479fa14dab17dcaa20375d3acb6ace5
S18RNGchampion <66316294+S18RNGchampion@users.noreply.github.com>
2021-04-22 20:46:22 +0800

    Delete LibraryCopy/src/sample/test directory

--- LibraryCopy/src/sample/test/Start.java
+++
@@ -1,9 +0,0 @@
-package sample.test;
-
-import javafx.application.Application;
-
-public class Start {
-    public static void main(String[] args) {
-        Application.launch(Test.class);
-    }
-}
--- LibraryCopy/src/sample/test/TestControl.java
+++
@@ -1,6 +0,0 @@
-package sample.test;
-
-import sample.Book;
-public class TestControl {
-
-}
